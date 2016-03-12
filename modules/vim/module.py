3fd67bf317f957a240baa354101211fd71dc37e7
Martin Tournoij <martin@arp242.net>
2016-03-12 19:04:35 +0100

    Also neovim symlink

--- modules/vim/module.py
+++ modules/vim/module.py
@@ -5,6 +5,9 @@ dirs = {
 symlinks = {
 	# Required for some older versions of Vim
 	'~/.vim/vimrc': '~/.vimrc',
+
+        # Neovim
+        '~/.vim/vimrc': '~/.vim/init.vim',
 }
 
 # TODO: Define custom run() function which copies spellfiles from dest to src if
