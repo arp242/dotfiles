1bbf275e6a34891baa18b14f9428996bfaa3d0d9
Martin Tournoij <martin@arp242.net>
2016-04-22 20:56:40 +0200

    Update

--- modules/x11/module.py
+++ modules/x11/module.py
@@ -1,13 +1,11 @@
 files = {
 	'~/.XCompose': 'XCompose',
-	'~/.Xdefaults ': 'Xdefaults',
-	'~/.Xmodmap ': 'Xmodmap',
-	'~/.fonts.conf ': 'fonts.conf',
-	'~/.gtkrc-2.0': 'gtkrc-2.0',
-	'~/.icons/default/index.theme': 'icons-default-index.theme',
-	'~/.xinitrc ': 'xinitrc',
-	'~/.xsession ': 'xsession',
-	'~/.wallpaper.png ': 'wallpaper.png',
+	'~/.Xdefaults': 'Xdefaults',
+	'~/.Xmodmap': 'Xmodmap',
+	'~/.fonts.conf': 'fonts.conf',
+	'~/.xinitrc': 'xinitrc',
+	'~/.xsession': 'xsession',
+	'~/.wallpaper.png': 'wallpaper.png',
 }
 
 dirs = {
