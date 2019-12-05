674e711b7a8f2a46dfa779108f1daa3d2cd0d288
Martin Tournoij <martin@arp242.net>
2019-12-05 22:40:03 +0800

    update

--- x11/module.py
+++ x11/module.py
@@ -1,11 +1,10 @@
 files = {
-	'~/.XCompose':                      'XCompose',
-	'~/.Xmodmap':                       'Xmodmap',
-	'~/.config/fontconfig/fonts.conf':  'fonts.conf',
-	'~/.xinitrc':                       'xinitrc',
+    '~/.XCompose':                      'XCompose',
+    '~/.Xmodmap':                       'Xmodmap',
+    '~/.config/fontconfig/fonts.conf':  'fonts.conf',
+    '~/.xinitrc':                       'xinitrc',
     '~/.icons/default/index.theme':     'icons',
     '~/.config/gtk-3.0/settings.ini':   'gtk3-settings.ini',
-    '~/.config/rofi/config':            'rofi.config',
 }
 
 dirs = {
